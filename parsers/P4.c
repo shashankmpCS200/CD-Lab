e1879b86b4bca1498bca6985e2e68944e1b9e6d0
Shashank M Patil <117842648+shashankmpCS200@users.noreply.github.com>
2024-02-01 10:30:46 +0530

    Create P4.c

---
+++ parsers/P4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+int S();
+char input[100];
+int currentIndex = 0;
+
+int match(char symbol) {
+    if (input[currentIndex] == symbol) {
+        currentIndex++;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int S() {
+    int tempIndex = currentIndex;
+
+    if (match('a') && match('a') && match('a')) {
+        if (S() && match('a') && match('a') && match('a')) {
+            return 1;
+        }
+    }
+
+    currentIndex = tempIndex;
+
+    if (match('a') && match('a')) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int main() {
+    printf("Enter the input string: ");
+    scanf("%s", input);
+
+    currentIndex = 0;
+
+    if (S() && currentIndex == strlen(input)) {
+        printf("Parsing successful! Input belongs to the given grammar.\n");
+    } else {
+        printf("Parsing failed! Input does not belong to the given grammar.\n");
+    }
+
+    return 0;
+}
